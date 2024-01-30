1f4cc4b6d97a0534e4adfbdfe532e4e4f70b2fb1
Berkan K <berkan0hk@gmail.com>
2024-01-30 21:12:06 +0300

    exercise 4 description

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -8,8 +8,21 @@ const removeFromArray = function (arr, ...values) {
   return arr;
 };
 
-removeFromArray([1, 2, 3, 4, 5, 6, 7, 7], 7, 4, 3);
-
-
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// ...values rest parametresi olarak kullanılıyor. bu sayede bir veya birden fazla değeri bir arrayden çıkarmak istediğimde tek tek her value için bir parametre oluşturmakla uğraşmak zorunda değilim. aynı zamanda .length metoduyla bir loop yazıp işin içinden sıyrılabilirim.
+
+// REST parametresinden önce bulduğum çözüm:
+
+// const removeFromArray = function (arr, value, value2) {
+//   let index = arr.indexOf(value);
+//   let index2 = arr.indexOf(value2);
+//   if (index > -1 && index2 > -1) {
+//     arr.splice(index, 1)
+//     arr.splice(index2, 1)
+//   }
+//   return arr;
+// }
+
+// removeFromArray([1, 2, 3, 4], 3, 2)
