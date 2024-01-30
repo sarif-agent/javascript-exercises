b308abd03b62d0ed72bd6805e2a2b2798ac8e112
Berkan K <berkan0hk@gmail.com>
2024-01-30 17:54:54 +0300

    exercise 2 done - all test pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) return 'ERROR';
+  let message = "";
+  for (i = 0; i < num; i++) {
+    message += str;
+  }
+  return message;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// Create a variable to hold the string you're going to return
