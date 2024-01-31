392d4fde54f83bdf37f9f4da31cb9fabd33d355d
Berkan K <berkan0hk@gmail.com>
2024-01-31 19:31:13 +0300

    exercise 5 done - all test pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function (value1, value2) {
+  if (value1 < 0 || value2 < 0) return "ERROR";
+  if (!Number.isInteger(value1) || !Number.isInteger(value2)) return "ERROR";
+  let n1 = Math.min(value1, value2);
+  let n2 = Math.max(value1, value2);
+  let sum = 0;
 
+  for (let i = n1; i <= n2; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
